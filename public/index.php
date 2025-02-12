2d6d7e42c398c77d850d8c2763f6fd9c906f53f4
Your Name <naldorsc2@gmail.com>
2025-02-12 21:35:50 +0100

    setup templates

--- public/index.php
+++ public/index.php
@@ -2,17 +2,30 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
+// Create Twig
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+$app->get('/', function ($request, $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'home.html.twig', [
+        'name' => 'John',
+    ]);
 });
+
 
 // Define app routes
 $app->get('/hello', function (Request $request, Response $response, $args) {
