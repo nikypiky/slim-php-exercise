349f48dcf677a4d4a7fe28d6220a3ccb824b63c1
Your Name <naldorsc2@gmail.com>
2025-02-14 23:56:46 +0100

    changed password now hashed

--- public/index.php
+++ public/index.php
@@ -152,17 +152,23 @@ $app->post('/edit-user/{id}', function ($request, $response, array $args) {
 		die ("Please choose a option.");
 	}
 	$chosen_field = $data['field'];
-	if ($chosen_field === 'username') $error_message = checkUsername($data['new_data'], $mysqli);
-	if ($chosen_field === 'email') $error_message = checkEmail($data['new_data'], $mysqli);
-	if ($chosen_field === 'password') $error_message = checkPassword($data['new_data'], $data['new_data'], $mysqli);
+	$new_data = $data['new_data'];
+	if ($chosen_field === 'username') $error_message = checkUsername($new_data, $mysqli);
+	if ($chosen_field === 'email') $error_message = checkEmail($new_data, $mysqli);
+	if ($chosen_field === 'password') $error_message = checkPassword($new_data, $new_data, $mysqli);
 
 	if ($error_message){
 		$response->getBody()->write($error_message);
 		return $response;
 	}
+
+	if ($chosen_field === 'password') {
+		$new_data = password_hash($new_data, PASSWORD_DEFAULT);
+	}
+
 	try {
 		$stmt = $mysqli->prepare("UPDATE users SET `$chosen_field` = ? WHERE id = ?");
-		$stmt->bind_param("si", $data["new_data"], $id);
+		$stmt->bind_param("si", $new_data, $id);
 		$stmt->execute();
 	} catch (\Throwable $th) {
 		$error_message = "Internal error, please try again later.";
