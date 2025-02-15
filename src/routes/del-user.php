8eedecf942ddda63bf7892dbc0e176d52da6e8e4
Your Name <naldorsc2@gmail.com>
2025-02-15 16:42:29 +0100

    finalised user deletion

--- src/routes/del-user.php
+++ src/routes/del-user.php
@@ -11,9 +11,15 @@ $app->delete('/del-user/{id}', function ($request, $response, array $args) {
 		$stmt = $mysqli->prepare("DELETE FROM users WHERE id = ?");
 		$stmt->bind_param("i", $id_int);
 		$stmt->execute();
+		$stmt = $mysqli->prepare("DELETE FROM admins WHERE admin_id = ?");
+		$stmt->bind_param("i", $id_int);
+		$stmt->execute();
 	} catch (\Throwable $th) {
 		$error_message = "Internal error, please try again later.";
 		return render_error(500, 'user-table.html.twig', $th, $request, $response);
 	}
-		return $response->withHeader('Location', '/user-table')->withStatus(302);
+	if ($id == $_SESSION['id']){
+		session_destroy();
+	}
+		return $response->withHeader('Location', '/')->withStatus(302);
 })->add(new CheckLoginMiddleware());
